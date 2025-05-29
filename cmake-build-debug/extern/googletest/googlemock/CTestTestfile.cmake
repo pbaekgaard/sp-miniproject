54014a603c12ede5b09085463ebff90805233b0f
pbaekgaard <pbakga21@student.aau.dk>
2025-05-29 13:57:09 +0200

    removed debug folder

--- cmake-build-debug/extern/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Tidligere_skrivebord/UNI_SOFTWARE/8_semester/STP/sp-miniproject/extern/googletest/googlemock
-# Build directory: C:/Tidligere_skrivebord/UNI_SOFTWARE/8_semester/STP/sp-miniproject/cmake-build-debug/extern/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/tests/Tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Tidligere_skrivebord/UNI_SOFTWARE/8_semester/STP/sp-miniproject/cmake-build-debug/tests/Tests[1]_tests.cmake")
-  include("C:/Tidligere_skrivebord/UNI_SOFTWARE/8_semester/STP/sp-miniproject/cmake-build-debug/tests/Tests[1]_tests.cmake")
-else()
-  add_test(Tests_NOT_BUILT Tests_NOT_BUILT)
-endif()
